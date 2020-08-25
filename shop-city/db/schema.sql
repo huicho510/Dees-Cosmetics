98ed104e7119b162550592b0409d7423d4068127
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-08-24 17:18:41 -0700

    changed stock to 255

--- shop-city/db/schema.sql
+++ shop-city/db/schema.sql
@@ -2,12 +2,12 @@ DROP IF EXISTS inventory_db;
 CREATE DATABASE inventory_db;
 USE inventory_db;
 
-CREATE TABLE inventory
+CREATE TABLE product_inventory
 (
 	id int NOT NULL AUTO_INCREMENT,
     product_title varchar(255) NOT NULL,
     price decimal(6, 2) NOT NULL,
-	stock int(999) NOT NULL,
+	stock int(255) NOT NULL,
     seller varchar(50) NOT NULL,
     product_condition varchar(4),
     details varchar(2000),
