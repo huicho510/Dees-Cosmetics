78b6a64f7a2130ac95aa62e2b789976f7bfcde14
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-08-24 17:45:22 -0700

    fixed seed

--- shop-city/db/seed.sql
+++ shop-city/db/seed.sql
@@ -1,10 +1,10 @@
 USE inventory_db;
 
-INSERT INTO inventory (product_title, price, stock, seller, condition, postedDate, createdAt, updatedAt, details)
-VALUES ("Apple Iphone SE", 899.99, 15, "Gerry Smith", "New", "7/17/2020", curdate(), curdate(), "Brand New Apple Iphone SE, does not come with Charger.");
+INSERT INTO product_inventory (product_title, price, stock, seller, product_condition, details, postedDate)
+VALUES ("Apple Iphone SE", 899.99, 15, "Gerry Smith", "New", "Brand New Apple Iphone SE, does not come with Charger.", curdate());
 
-INSERT INTO inventory (product_title, price, stock, seller, condition, postedDate, createdAt, updatedAt, details)
-VALUES ("Flintstones Gummy Vitamins Men", 15.99, 100, "David Blaine", "Used", "9/15/2010", curdate(), curdate(), "Opened container of Flintstones Gummy Vitamins for Men, I only ate one!");
+INSERT INTO product_inventory (product_title, price, stock, seller, product_condition, details, postedDate)
+VALUES ("Flintstones Gummy Vitamins Men", 15.99, 100, "David Blaine", "Used", "Opened container of Flintstones Gummy Vitamins for Men, I only ate one!", curdate());
 
-INSERT INTO inventory (product_title, price, stock, seller, condition, postedDate, createdAt, updatedAt, details)
-VALUES ("Used Sony PSP", 80.50, 1, "Supero Market", "Used", "1/01/2017", curdate(), curdate(), "Found a PSP on the ground yesterday, hopefully it works! First come, first serve.");
+INSERT INTO product_inventory (product_title, price, stock, seller, product_condition, details, postedDate)
+VALUES ("Used Sony PSP", 80.50, 1, "Supero Market", "Used", "Found a PSP on the ground yesterday, hopefully it works! First come, first serve.", curdate());
