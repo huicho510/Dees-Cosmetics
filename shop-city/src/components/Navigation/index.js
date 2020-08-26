454f1a5a5cdbebe054ff0b0fabe53ebe3f04e1e5
Luis Cervantes <luiscerv1229@gmail.com>
2020-08-26 15:24:51 -0700

    made Nav bar with React-bootstrap

--- shop-city/src/components/Navigation/index.js
+++ shop-city/src/components/Navigation/index.js
@@ -17,7 +17,7 @@ function Navigation() {
     <Navbar.Brand href="#home">Shop City</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#features">Shop</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     
     <Form inline>
