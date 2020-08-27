0e898ec29bbc251fede545c7ace12dcf07dcb63a
Luis Cervantes <luiscerv1229@gmail.com>
2020-08-26 18:18:04 -0700

    added sticky footer and a card(card still needs work)

--- shop-city/src/App.js
+++ shop-city/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navigation from "./components/Navigation/index";
+import Footer from "./components/Footer/Footer"
+import Card from "./components/Card/index"
 
 
 
@@ -10,6 +12,8 @@ function App() {
     <Router>
       <div>
         <Navigation />
+        <Card />
+        <Footer />
         </div>
         </Router>
   );
--- shop-city/src/components/Navigation/Drawer.js
+++ shop-city/src/components/Navigation/Drawer.js
@@ -7,7 +7,7 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu'
+
 
 
 const useStyles = makeStyles({
