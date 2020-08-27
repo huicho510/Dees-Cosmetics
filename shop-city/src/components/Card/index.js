1dd63d7ba09772cbbd9bcd506c4285f765f69746
Luis Cervantes <luiscerv1229@gmail.com>
2020-08-26 20:47:29 -0700

    added card component/not final product/needs work

--- shop-city/src/components/Card/index.js
+++ shop-city/src/components/Card/index.js
@@ -1,50 +1,37 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import Card from 'react-bootstrap/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import Button from 'react-bootstrap/Button';
 import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth:345,
   },
   media: {
     height: 140,
   },
 });
 
-export default function MediaCard() {
-  const classes = useStyles();
+export default function MediaCard(props) {
+ 
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Mteal Straw
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Metal Straws will save the sea Turtles and you can still drink comfortably
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-         Add to Cart
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={props.image} />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button size="small" variant='secondary'>Add to Cart</Button>
+  </Card.Body>
+</Card>
   );
 }
