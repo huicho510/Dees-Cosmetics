435bff4ec77d2aa4d6207adcd0aa5e164bd7aca7
Luis Cervantes <luiscerv1229@gmail.com>
2020-09-02 18:02:06 -0700

    coverted Banner into its own component

--- shop-city/client/src/components/FormUserDetails/index.js
+++ shop-city/client/src/components/FormUserDetails/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import TextField from "material-ui/TextField";
 import Navbar from "react-bootstrap/Navbar";
-import Button from '@material-ui/core/Button';
-import "./index.css"
-
+import Button from "@material-ui/core/Button";
+import "./index.css";
 
 function FormUserDetails(props) {
-
-
-
   const forward = (e) => {
     e.preventDefault();
     props.nextStep();
@@ -17,70 +13,64 @@ function FormUserDetails(props) {
 
   const { values, handleChange } = props;
 
-
-    
-
-    return (
-      <div className="content">
-        <MuiThemeProvider>
-          <React.Fragment>
-            <Navbar
-              variant="dark"
-              className="justify-content-center"
-            >
-              <h1 className='text-dark'>Enter User Details</h1>
-            </Navbar>
-            <TextField
-              hintText="Enter Your First Name"
-              floatingLabelText="First Name"
-              onChange={handleChange("firstName")}
-              defaultValue={values.firstName}
-            />
-            <br />
-            <TextField
-              hintText="Enter Your Last Name"
-              floatingLabelText="Last Name"
-              onChange={handleChange("lastName")}
-              defaultValue={values.lastName}
-            />
-            <br />
-            <TextField
-              hintText="Enter Your email"
-              floatingLabelText="Email"
-              onChange={handleChange("email")}
-              defaultValue={values.email}
-            />
-            <br />
-            <TextField
-              hintText="Enter Your password"
-              floatingLabelText="Password"
-              onChange={handleChange("password")}
-              defaultValue={values.password}
-            />
-            <br />
-            <Button
-              className="button"
-              label="Continue"
-             
-              style={styles.button}
-              onClick={forward}>Continue
-              </Button>
-          </React.Fragment>
-        </MuiThemeProvider>
-      </div>
-    );
-  }
-
+  return (
+    <div className="content">
+      <MuiThemeProvider>
+        <React.Fragment>
+          <Navbar variant="dark" className="justify-content-center">
+            <h1 className="text-dark">Enter User Details</h1>
+          </Navbar>
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange("lastName")}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your email"
+            floatingLabelText="Email"
+            onChange={handleChange("email")}
+            defaultValue={values.email}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your password"
+            floatingLabelText="Password"
+            onChange={handleChange("password")}
+            defaultValue={values.password}
+          />
+          <br />
+          <Button
+            className="button"
+            label="Continue"
+            style={styles.button}
+            onClick={forward}
+          >
+            Continue
+          </Button>
+        </React.Fragment>
+      </MuiThemeProvider>
+    </div>
+  );
+}
 
 const styles = {
   button: {
-    background: 'linear-gradient(45deg, #fc5296 20%, #f67062 90%)',
+    background: "linear-gradient(45deg, #fc5296 20%, #f67062 90%)",
     borderRadius: 3,
     border: 0,
-    color: 'white',
+    color: "white",
     height: 45,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(250, 105, 135, .3)',
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(250, 105, 135, .3)",
   },
 };
 
