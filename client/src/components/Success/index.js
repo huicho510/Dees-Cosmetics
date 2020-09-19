0cd9a7fcadb3831d3e0c78111503ca91a4e134be
Luis Cervantes <luiscerv1229@gmail.com>
2020-09-18 18:25:29 -0700

    added button to success page that takes you back home

--- client/src/components/Success/index.js
+++ client/src/components/Success/index.js
@@ -1,9 +1,11 @@
 import React  from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import "./style.css"
+import { useHistory } from "react-router-dom";
 
 function Success() {
  
+  const history = useHistory();
     return (
         <div className="success">
       <MuiThemeProvider>
@@ -11,6 +13,8 @@ function Success() {
             <h1 className="text-dark">Success</h1>
         
           <h4>You Will Get an Email with Further instructions</h4>
+
+          <button className= "shop-btn" onClick={e => history.push('/home')}> Start Shopping</button>
         </React.Fragment>
       </MuiThemeProvider>
       </div>
